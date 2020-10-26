98f1922d8fd3fcf1b345a0c5ed67294e5d991764
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-10-26 17:16:40 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from flask import Flask, request, Response
+import os
+from flask_cors import CORS
+from bson.json_util import loads, dumps, ObjectId
+import pandas as pd
+
+client = MongoClient(os.environ.get("MONGO_CONNECTION_STRING"))
+# client = MongoClient() # For offline testing.
+db = client.test
+visualizations = db.visualizations
+
+DEBUG = True
+app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})  # enable CORS
+
+@app.route('/config', methods=['GET', 'POST'])
+def respond_config():
+  if request.form['url'] != 'undefined':
+    db_entry_id = ObjectId(loads(request.form['url']))
+    db_entry = db.visualizations.find_one({"_id": db_entry_id})
+    try:
+      data = pd.read_parquet(BytesIO(db_entry['filtered_dataframe'])).to_json(orient='records')
+    except:
+      if type(db_entry['transformed_dataframe']) == bytes: # The mockup db_entry stores the empty transformed_dataframe as a list, so don't convert that one.
+        data = pd.read_parquet(BytesIO(db_entry['transformed_dataframe'])).to_json(orient='records')
+      else:
+        data = db_entry['transformed_dataframe']
+  return Response(data, mimetype="application/json")
+
+client.close()
