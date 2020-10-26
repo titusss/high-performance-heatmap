da4420866a148c64a94947f57c682ca556c2143d
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-10-26 18:45:55 +0100

    Added missing import. Changed to_json orientation.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from flask_cors import CORS
 from bson.json_util import loads, dumps, ObjectId
 import pandas as pd
+from io import BytesIO
 
 client = MongoClient(os.environ.get("MONGO_CONNECTION_STRING"))
 # client = MongoClient() # For offline testing.
@@ -24,10 +25,10 @@ def respond_config():
     db_entry_id = ObjectId(loads(request.form['url']))
     db_entry = db.visualizations.find_one({"_id": db_entry_id})
     try:
-      data = pd.read_parquet(BytesIO(db_entry['filtered_dataframe'])).to_json(orient='records')
+      data = pd.read_parquet(BytesIO(db_entry['filtered_dataframe'])).to_json(orient='index')
     except:
       if type(db_entry['transformed_dataframe']) == bytes: # The mockup db_entry stores the empty transformed_dataframe as a list, so don't convert that one.
-        data = pd.read_parquet(BytesIO(db_entry['transformed_dataframe'])).to_json(orient='records')
+        data = pd.read_parquet(BytesIO(db_entry['transformed_dataframe'])).to_json(orient='index')
       else:
         data = db_entry['transformed_dataframe']
   return Response(data, mimetype="application/json")
