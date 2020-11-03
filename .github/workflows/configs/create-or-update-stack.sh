a479e04f77bf227c4557fe2f7c9a83f80a163fec
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-11-03 17:13:48 +0100

    Delete create-or-update-stack.sh

--- .github/workflows/configs/create-or-update-stack.sh
+++
@@ -1,4 +0,0 @@
-set -e -x 
-STACK_EXISTS=`openstack stack list |grep $STACK_NAME | wc -l`
-if [ $STACK_EXISTS == 0 ]; then METHOD=create  ; else METHOD=update ; fi
-openstack stack $METHOD -e $STACK_PARS -t $STACK_FILE $STACK_NAME -fjson
